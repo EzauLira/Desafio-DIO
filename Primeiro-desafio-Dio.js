8d691c84ce73613f3889f33db701cdabf46d8ff2
ezaulira <ezaumiguellira23@gmail.com>
2023-12-12 19:02:55 -0300

    first commit

--- Primeiro-desafio-Dio.js
+++ Primeiro-desafio-Dio.js
@@ -1,5 +1,5 @@
 //Variável com nome e Quantidade de XP do Pokemon
-let nomePokemon = "Charmander";
+let nomePokemon = "Pikachu";
 let xpPokemon = 12000;
 
 //Estrutura de switch/Case para determinar o nível do herói com base na quantidade de XP
@@ -7,25 +7,25 @@ let nivelPokemon;
 
 switch (true) {
     case xpPokemon < 1000:
-        nivelPokemon = "0 á 2 = FERRO," + " Continue evoluindo!";
+        nivelPokemon = "0 á 2 e no Rank: FERRO," + " Continue evoluindo!";
         break;
     case xpPokemon >= 1001 && xpPokemon <= 2000:
-        nivelPokemon = "2 a 3 = BRONZE," + " Você está ficando forte!";
+        nivelPokemon = "2 a 3 e no Rank: BRONZE," + " Você está ficando forte!";
         break;
     case xpPokemon >= 2001 && xpPokemon <= 5000:
-        nivelPokemon = "3 a 5 = PRATA," + " a jornada é longa... Mas não para os guerreiros!";
+        nivelPokemon = "3 a 5 e no Rank: PRATA," + " a jornada é longa... Mas não para os guerreiros!";
         break;
     case xpPokemon >= 5001 && xpPokemon <= 7000:
-        nivelPokemon = "5 a 6 = OURO," + " Sua evolução é notória!";
+        nivelPokemon = "5 a 6 e no Rank: OURO," + " Sua evolução é notória!";
         break;
     case xpPokemon >= 7001 && xpPokemon <= 8000:
-        nivelPokemon = "6 a 8 = PLATINA," + " Falta pouco para o poder máximo!";
+        nivelPokemon = "6 a 8 e no Rank: PLATINA," + " Falta pouco para o poder máximo!";
         break;
     case xpPokemon >= 8001 && xpPokemon <= 9000:
-        nivelPokemon = "8 a 9 = ASCENDENTE," + " Realmente você é muito forte!";
+        nivelPokemon = "8 a 9 e no Rank: ASCENDENTE," + " Realmente você é muito forte!";
         break;
     case xpPokemon >= 9001 && xpPokemon <= 10000:
-        nivelPokemon = "9 a 10 = Imortal," + " Incrível você superou seu mestre!";
+        nivelPokemon = "9 a 10 e no Rank: Imortal," + " Incrível você superou seu mestre!";
         break;
     default:
         nivelPokemon = "10+ que coloca ele no rank: RADIANTE!!! " + " Nada mais te para!";
